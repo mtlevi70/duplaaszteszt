be1ad63ad2a9085dcb1b727d451c101d0e7c29f9
mtlevi70 <levente.hegyi@stud.uni-corvinus.hu>
2025-08-12 01:00:54 +0200

    index.html teljesen mukodik, termekek.html alap

---
+++ products_help.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let help_button = document.querySelectorAll('.imagebox h3 span img');
+
+    let help = document.querySelectorAll('.imagebox .help');
+    let isOpen = false;
+
+    
+    help_button.forEach(function(clicked){
+        clicked.addEventListener('click', function() {
+            let help_button_id = parseInt(clicked.id);
+            console.log(help_button_id);
+            if (!isOpen){
+                help[help_button_id].classList.add('active');
+                isOpen = true;
+            }
+            else{
+                help[help_button_id].classList.remove('active');
+                isOpen = false;
+            } 
+        });
+    });
+    
+  /*   help_button.addEventListener('click', function () {
+        let help_button_id = element.getAttribute('id');
+        console.log(help_button_id);
+        if (!isOpen){
+            help.classList.add('active');
+            isOpen = true;
+        }
+        else{
+            help.classList.remove('active');
+            isOpen = false;
+        } 
+
+    });
+ */
+});
